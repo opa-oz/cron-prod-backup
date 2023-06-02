afa53a3f4239d5b990324048e0ef5ed95a6df22a
Vladimir Levin <vladimirlevinozinki@gmail.com>
2023-06-02 16:46:09 +0900

    v0.1.0

--- cron-prod-backup/main.py
+++ cron-prod-backup/main.py
@@ -1,2 +1,32 @@
+import tarfile
+import os.path
+from pathlib import Path
+
+targets = [
+    "calibre",
+    "gitea",
+    "gitea-act",
+    "drone",
+    "n8n",
+    "pihole",
+    "jellyfin",
+]
+
+source = Path("/source")
+dest = Path("/destination")
+
+
+def make_tarfile(output_filename: Path, source_dir: Path):
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+
+def main():
+    for target in targets:
+        print(f"Start {target}")
+        make_tarfile(dest / f"{target}.tar.gz", source / target)
+        print(f"Finish {target}")
+
+
 if __name__ == "__main__":
-    print("Hello, world!")
+    main()
